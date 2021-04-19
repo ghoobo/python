f3e30dc9328ec617260dbe61558efface414bd12
ghoobo <ghoobo@163.com>
2021-04-19 22:44:07 +0800

    python 循环

--- com/ghoobo/basis/test_09_iter.py
+++ com/ghoobo/basis/test_09_iter.py
@@ -41,3 +41,21 @@ class test_iter(unittest.TestCase):
                 print(next(it2), end=" ")
             except StopIteration:
                 sys.exit()  # 当try块中StopIteration异常时，执行
+
+    def fibonacci(self,n):  # 生成器函数 - 斐波那契
+        a, b, counter = 0, 1, 0
+        while True:
+            if (counter > n):
+                return
+            yield a
+            a, b = b, a + b
+            counter += 1
+
+    def test_yield(self):
+        f = self.fibonacci(10)  # f 是一个迭代器，由生成器返回生成
+
+        while True:
+            try:
+                print(next(f), end=" ")
+            except StopIteration:
+                sys.exit()
